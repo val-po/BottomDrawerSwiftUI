a84c82026d38767d96257b94cd9f71ed60a575a5
VP <VP>
2021-11-10 21:33:15 +0200

    Initial Commit

---
+++ BottomDrawerSwiftUI/BottomDrawerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomDrawerSwiftUIApp.swift
+//  BottomDrawerSwiftUI
+//
+//  Created by valentine on 10.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct BottomDrawerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
